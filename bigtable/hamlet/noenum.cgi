2eb19ea4ab360ba71e7ae81e67f875e554add9bb
Michael Snoyman <michael@snoyman.com>
2011-01-12 14:48:06 +0200

    Removed old benchmarks

--- bigtable/hamlet/noenum.cgi
+++
@@ -1,1 +0,0 @@
-noenum/hamlet.cgi
--- hello/fastcgi/hello.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-import Network.Wai
-import Network.Wai.Handler.FastCGI
-main = run $ const $ return Response
-    { responseBody = ResponseLBS "Hello World"
-    , status = status200
-    , responseHeaders = [("Content-Type", "text/plain")]
-    }
