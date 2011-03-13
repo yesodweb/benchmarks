1a63ccb10de69452117bdacecaab8b76346558a3
Michael Snoyman <michael@snoyman.com>
2011-03-13 09:28:54 +0200

    Added PHP PONG

---
+++ pong/php.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+d=$(readlink -f $(dirname $0))
+
+php-fpm -n
+
+/usr/local/nginx/sbin/nginx -c $d/php.nginx.conf &
+nx=$!
+
+httperf --hog --server=localhost --port=3000 --uri=/ --rate=1000 --num-conns=200 --num-calls=100 --burst-length=20
+kill $nx
+
+killall -KILL php-fpm
