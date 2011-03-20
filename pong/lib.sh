2a9f5436fb0912fe1b5f284b3e5e19022fc5ced4
Michael Snoyman <michael@snoyman.com>
2011-03-20 10:29:01 +0200

    More intense test case

--- pong/lib.sh
+++ pong/lib.sh
@@ -1,7 +1,7 @@
 benchmark(){
   sleep 5
   nx=$!
-  httperf --hog --server=localhost --port=3000 --uri=/ --rate=1000 --num-conns=200 --num-calls=100 --burst-length=20 > results/`basename $0 .sh`
+  httperf --hog --server=localhost --port=3000 --uri=/ --rate=1000 --num-conns=1000 --num-calls=1000 --burst-length=20 > results/`basename $0 .sh`
   kill $nx
   sleep 2
 }
