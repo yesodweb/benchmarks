6cbed69030001aab0307216ca8af57dc531a0c94
benma <mbencun@gmail.com>
2013-04-17 01:47:16 +0300

    node.js: utilize three cores
    node.js script modified to use three cores to enable a fairer comparison (the Haskell programs are also run with `+RTS -N3`).

--- pong/node.js
+++ pong/node.js
@@ -1,5 +1,13 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('PONG');
-}).listen(3000, "127.0.0.1");
+var cluster = require('cluster');
+
+if(cluster.isMaster) {
+  for(var i = 0; i < 3; i++) {
+    cluster.fork();
+  }
+} else {
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('PONG');
+  }).listen(3000, "127.0.0.1");
+}
